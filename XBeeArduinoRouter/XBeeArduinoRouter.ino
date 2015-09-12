9c5f00427c8db9465410cbdf1ad5d46bb26f7386
ghirlekar <gaurav.hirlekar@gmail.com>
2015-09-12 14:47:01 -0400

    Code cleanup and style matching
    Cleaned up coordinator code and matched style with router code.
    
    Added input command handling capability in coordinator code.

--- XBeeArduinoRouter/XBeeArduinoRouter.ino
+++ XBeeArduinoRouter/XBeeArduinoRouter.ino
@@ -1,49 +1,49 @@
 #include <math.h>
+
 const String deviceID = "0xff3b";
+const String broadcastID = "0xffff";
+const int thermPin = A0;
 
-String inputString = "";
-boolean stringComplete = false, sync = false;
-
-const int tempPin = A0;
 double Vout, Rth, temp;
 unsigned long timestamp = 0, now, period = 1000;
+String serialStr = "";
+boolean serialStrComplete = false, sync = false;
+char inChar;
 
 void setup() {
   Serial.begin(9600);
-  pinMode(tempPin, INPUT);
+  pinMode(thermPin, INPUT);
 }
 
 void loop() {
+  while (Serial.available()) {
+    inChar = (char) Serial.read();
+    if (inChar == '\n') {
+      serialStrComplete = true;
+      break;
+    } else serialStr += inChar;
+  }
+
+  if (serialStrComplete) {
+    if (serialStr.startsWith(deviceID) || serialStr.startsWith(broadcastID)) {
+      serialStr = serialStr.substring(7);
+      if (serialStr.startsWith("SYNC")) {
+        sync = true;
+      } else if (serialStr.startsWith("SET")) {
+        period = serialStr.substring(3).toInt();
+      }
+    }
+    serialStrComplete = false;
+    serialStr = "";
+  }
+
   now = millis();
   if ((now - timestamp) > period || sync) {
     sync = false;
     timestamp = now;
-    Vout = analogRead(tempPin) / 1024.0 * 5;
+    Vout = analogRead(thermPin) / 1024.0 * 5;
     Rth = (50000 - 10000 * Vout) / Vout;
     temp = 1 / (0.001129148 + 0.000234125 * log(Rth) + 8.76741E-08 * pow(log(Rth), 3)) - 273.15;
-    Serial.print(deviceID);
-    Serial.print(',');
-    Serial.println(temp, 1);
-  }
-  if (stringComplete) {
-    if (inputString.startsWith(deviceID) || inputString.startsWith("0xffff")) {
-      inputString = inputString.substring(7);
-      if (inputString.startsWith("SYNC")) {
-        sync = true;
-      } else if (inputString.startsWith("SET")) {
-        period = inputString.substring(3).toInt();
-      }
-    }
-    stringComplete = false;
-    inputString = "";
-  }
-}
-
-void serialEvent() {
-  while (Serial.available()) {
-    char inChar = (char) Serial.read();
-    inputString += inChar;
-    if (inChar == '\n')
-      stringComplete = true;
+    Serial.println(String(deviceID + "," + String(temp,1)));
   }
 }
