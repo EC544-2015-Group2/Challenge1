e6fac0726b88f5288aea9177fde61b10b172b533
ghirlekar <gaurav.hirlekar@gmail.com>
2015-09-15 11:32:04 -0400

    Removed intro message and added API to set sensor parameters
    I removed the introduction message that the Xbee broadcasts when it
    connects to the network as I believe this message and the data it
    contains are better set from UI connected to the base station instead
    of individually programming each sensor. To that end, now every sensor
    broadcasts a stream of comma separated sensor values, and it is up to
    the base station to make sense of which key each value should be paired
    with.
    
    Also, I added an API that reads the first byte of each received message
    to set sensor parameters such as synchronization and time period
    between readings along with whatever needs may come up in the future.

--- XBeeArduinoRouter/XBeeArduinoRouter.ino
+++ XBeeArduinoRouter/XBeeArduinoRouter.ino
@@ -1,41 +1,40 @@
-#include <Printers.h>
 #include <XBee.h>
-
 #include <math.h>
 
-
 XBee xbee = XBee();
-XBeeResponse response = XBeeResponse();
-//ZBRxResponse rx = ZBRxResponse();
-ModemStatusResponse msr = ModemStatusResponse();
+ZBRxResponse rx = ZBRxResponse();
 
-uint8_t introPayload[] = {'{', 't', 'e', 'm', 'p', '}'};  
 uint8_t dataPayload[5];
 
 XBeeAddress64 coordAddr64 = XBeeAddress64(0x00000000, 0x00000000);
-ZBTxRequest zbIntroTx = ZBTxRequest(coordAddr64, introPayload, sizeof(introPayload));
 ZBTxRequest zbDataTx = ZBTxRequest(coordAddr64, dataPayload, sizeof(dataPayload));
 
 const int thermPin = A0;
 
 double Vout, Rth, temp;
 unsigned long timestamp = 0, now, period = 1000;
-boolean introMessageSent = false, sync = false;
+boolean sync = false;
 
 void setup() {
   Serial.begin(9600);
   xbee.begin(Serial);
-  xbee.send(zbIntroTx);
-  //  pinMode(thermPin, INPUT);
+  pinMode(thermPin, INPUT);
 }
 
 void loop() {
   xbee.readPacket();
   if (xbee.getResponse().isAvailable()) {
-    if (xbee.getResponse().getApiId() == MODEM_STATUS_RESPONSE) {
-      xbee.getResponse().getModemStatusResponse(msr);
-      if (msr.getStatus() == ASSOCIATED)
-        xbee.send(zbIntroTx);
+    if (xbee.getResponse().getApiId() == ZB_RX_RESPONSE) {
+      xbee.getResponse().getZBRxResponse(rx);
+      switch(rx.getData(0)){
+        case 0xB0:  //This signals a SYNC command
+          sync = true;
+          break;
+        case 0xB1:  //This signals a command to set reporting period
+          uint8_t* rxPayload = rx.getData();
+          period = atoi((char*)++rxPayload);
+          break;
+      }
     }
   }
 
@@ -46,9 +45,7 @@ void loop() {
     Vout = analogRead(thermPin) / 1024.0 * 5;
     Rth = (50000 - 10000 * Vout) / Vout;
     temp = 1 / (0.001129148 + 0.000234125 * log(Rth) + 8.76741E-08 * pow(log(Rth), 3)) - 273.15;
-    char tempArr[4];
-    String(temp,1).toCharArray(tempArr,5);
-    memcpy(dataPayload, tempArr, 5);
+    String(temp, 1).toCharArray((char*)((void*)dataPayload), 5);
     xbee.send(zbDataTx);
   }
 }
