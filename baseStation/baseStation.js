e72f4aa38361486117a315786a5ed0fd78562825
ghirlekar <gaurav.hirlekar@gmail.com>
2015-09-15 13:17:25 -0400

    Added MQTT publish capability to base station and code cleanup to improve readability
    Also changed the code to write to database and publish to MQTT client
    only if they are connected.

--- baseStation/baseStation.js
+++ baseStation/baseStation.js
@@ -1,43 +1,89 @@
 var xbee_api = require('xbee-api'),
     serialPort = require('serialport'),
     MongoClient = require('mongodb').MongoClient,
-    assert = require('assert');
+    mqtt = require('mqtt');
+
+var xbeeAPIOptions = {
+    api_mode: 2
+};
+
+var serialOptions = {
+    baudrate: 9600,
+    openImmediately: true
+};
 
 var mongoURL = 'mongodb://localhost:27017/ZigBeeBaseStation';
+var mqttURL = 'mqtt://broker.mqtt-dashboard.com';
+var mqttTopicPrefix = 'EC544-Group2-Challenge1/temperature/';
 
 
 serialPort.list(function(err, ports) {
-    assert.equal(null, err);
-    var portName = ports.filter(function(port) {
-        if (port.comName.match('/dev/cu.u')) {
-            return true;
-        } else return false;
-    })[0].comName.replace('cu', 'tty');
+    if (err) console.log('Failed to read list of serial ports');
+    else {
+        var filteredPortList = ports.filter(portnameFilter);
 
-    var xbeeAPI = new xbee_api.XBeeAPI({
-        api_mode: 2
-    });
+        var xbeeAPI = new xbee_api.XBeeAPI(xbeeAPIOptions);
+        serialOptions.parser = xbeeAPI.rawParser();
 
-    var Serial = new serialPort.SerialPort(portName, {
-        baudrate: 9600,
-        parser: xbeeAPI.rawParser()
-    }, true, function() {
-        console.log('Opened serial port');
-        var C = xbee_api.constants;
-        var nodes = {};
+        var Serial = new serialPort.SerialPort(getPortName(filteredPortList, 0), serialOptions, serialOptions.openImmediately, function() {
+            console.log('Opened serial port');
+            var C = xbee_api.constants;
+            var databaseConnected = false;
+            var mqttConnected = false;
 
-        MongoClient.connect(mongoURL, function(err, db) {
-            console.log('Connected to database server')
+            console.log('Connecting to database server');
+            MongoClient.connect(mongoURL, function(err, db) {
+                if (err) console.log('Failed to connect to database server. Proceeding without database ....');
+                else {
+                    console.log('Connected to database server');
+                    databaseConnected = true;
+                }
+            });
+
+            var mqttClient = mqtt.connect(mqttURL);
+            console.log('Connecting to MQTT server');
+            mqttClient.on('connect', function() {
+                console.log('Connected to MQTT server');
+                mqttConnected = true;
+            });
+
             xbeeAPI.on('frame_object', function(frame) {
-                db.collection('temperature').insertOne({
-                    deviceID: frame.remote64,
-                    time: Date.now(),
-                    value: parseFloat(frame.data.toString('ascii'))
-                }, function(err, result) {
-                    assert.equal(null, err);
-                    console.log('Inserted a document into temperature collection');
-                });
+                data = buildDocument(frame);
+                if (databaseConnected) insertDocument(data, db, 'temperature');
+                if (mqttConnected) publishMQTT(data, mqttClient, mqttTopicPrefix + data.deviceID);
+                if (!databaseConnected && !mqttConnected) console.log('<<', frame);
             });
         });
-    });
+    }
 });
+
+function portnameFilter(port) {
+    if (port.comName.match('/dev/cu.u')) {
+        return true;
+    } else return false;
+};
+
+function getPortName(portList, index) {
+    return portList[index].comName.replace('cu', 'tty');
+}
+
+function buildDocument(APIframe) {
+    return {
+        deviceID: APIframe.remote64,
+        time: Date.now(),
+        value: parseFloat(APIframe.data.toString('ascii'))
+    };
+}
+
+function insertDocument(doc, db, collect) {
+    db.collection(collect).insertOne(doc, function(err, result) {
+        if (err) console.log('Error in inserting document')
+        else console.log('Inserted ' + result.insertCount + ' documents in collection');
+    });
+}
+
+function publishMQTT(doc, mqttClient, topic) {
+    mqttClient.publish(topic, JSON.stringify(doc), function() {
+        console.log('Published MQTT message to topic: ' + topic);
+    });
+}
